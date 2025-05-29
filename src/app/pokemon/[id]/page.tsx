75050a0120bb4bdfeb55f49eeec0b1c4daf15cb3
Gabriel Magdugo <magdugogabriel@gmail.com>
2025-05-29 21:30:23 +0800

    added filter type

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -4,120 +4,135 @@ import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 import Image from "next/image";
 import { poppins } from "@/utils/fonts";
-import { getWeakness,getPokemonDetails } from "@/utils/pokemon";
+import { getWeakness, getPokemonDetails } from "@/utils/pokemon";
 
 const typeColors: { [key: string]: string } = {
-    normal: "bg-gray-400",
-    fire: "bg-red-500",
-    water: "bg-blue-500",
-    electric: "bg-yellow-400",
-    grass: "bg-green-500",
-    ice: "bg-blue-200",
-    fighting: "bg-red-700",
-    poison: "bg-purple-500",
-    ground: "bg-yellow-700",
-    flying: "bg-indigo-300",
-    psychic: "bg-pink-500",
-    bug: "bg-lime-500",
-    rock: "bg-yellow-800",
-    ghost: "bg-purple-700",
-    dragon: "bg-indigo-700",
-    dark: "bg-gray-800",
-    steel: "bg-gray-500",
-    fairy: "bg-pink-300",
-    };
+  normal: "bg-gray-400",
+  fire: "bg-red-500",
+  water: "bg-blue-500",
+  electric: "bg-yellow-400",
+  grass: "bg-green-500",
+  ice: "bg-blue-200",
+  fighting: "bg-red-700",
+  poison: "bg-purple-500",
+  ground: "bg-yellow-700",
+  flying: "bg-indigo-300",
+  psychic: "bg-pink-500",
+  bug: "bg-lime-500",
+  rock: "bg-yellow-800",
+  ghost: "bg-purple-700",
+  dragon: "bg-indigo-700",
+  dark: "bg-gray-800",
+  steel: "bg-gray-500",
+  fairy: "bg-pink-300",
+};
 
-    function PokemonDetail() {
-    const router = useRouter();
-    const params = useParams();
-    const [pokemon, setPokemon] = useState<any>(null);
-    
+function PokemonDetail() {
+  const router = useRouter();
+  const params = useParams();
+  const [pokemon, setPokemon] = useState<any>(null);
 
-    useEffect(() => {
+  useEffect(() => {
     async function fetchPokemon() {
-        try {
-        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params?.id}`);
+      try {
+        const res = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${params?.id}`
+        );
         if (!res.ok) {
-            router.push("/not-found");
-            return;
+          router.push("/not-found");
+          return;
         }
         const data = await res.json();
         setPokemon(data);
-        } catch (error) {
+      } catch (error) {
         console.error("Failed to fetch Pokémon", error);
         router.push("/not-found");
-        }
+      }
     }
 
     if (params?.id) {
-        fetchPokemon();
+      fetchPokemon();
     }
-    }, [params?.id, router]);
+  }, [params?.id, router]);
 
-    if (!pokemon) return <p className="text-center mt-20">Loading...</p>;
+  if (!pokemon) return <p className="text-center mt-20">Loading...</p>;
 
-    const formattedId = String(pokemon.id).padStart(3, "0");
-    const Weaknesses = getWeakness(pokemon.types.map((typeObj: any) => typeObj.type.name));
+  const formattedId = String(pokemon.id).padStart(3, "0");
+  const Weaknesses = getWeakness(
+    pokemon.types.map((typeObj: any) => typeObj.type.name)
+  );
 
-    return (
-    <div className={`flex flex-col items-center ${poppins.className}`}>
-        <div className="m-10 rounded-2xl">
+  return (
+    <div className={`flex flex-col items-center m-10 ${poppins.className}`}>
+      <div className="absolute left-20 top-20">
+        <button
+          onClick={() => {
+            router.push("/");
+          }}
+          className="bg-white p-5 rounded-full  hover:scale-115 transition-transform duration-300 shadow-lg"
+        >
+          Back
+        </button>
+      </div>
+      <div className="m-10 rounded-2xl">
         <Image
-            src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formattedId}.png`}
-            alt={`Photo of ${pokemon.name}`}
-            width={400}
-            height={400}
+          src={`https://assets.pokemon.com/assets/cms2/img/pokedex/full/${formattedId}.png`}
+          alt={`Photo of ${pokemon.name}`}
+          width={400}
+          height={400}
         />
         <div className="bg-white rounded-2xl opacity-75 p-4 mt-4 text-center">
-            <h1 className="capitalize text-2xl font-bold">{pokemon.name}</h1>
-            <p className="text-xl">#{formattedId}</p>
-            <div className="flex gap-2 justify-center mt-2">
+          <h1 className="capitalize text-2xl font-bold">{pokemon.name}</h1>
+          <p className="text-xl">#{formattedId}</p>
+          <div className="flex gap-2 justify-center mt-2">
             {pokemon.types.map((typeObj: any) => (
-                <span
+              <span
                 key={typeObj.type.name}
-                className={`px-3 py-1 rounded-full text-white ${typeColors[typeObj.type.name]}`}
-                >
+                className={`px-3 py-1 rounded-full text-white ${
+                  typeColors[typeObj.type.name]
+                }`}
+              >
                 {typeObj.type.name}
-                </span>
+              </span>
             ))}
-            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="flex flex-col gap-4 items-center w-full max-w-xs opacity-75">
+        <div className="bg-white rounded-2xl p-4 w-full text-center">
+          <h1 className="text-2xl font-bold text-center">Stats</h1>
+          <ul className="mt-2">
+            {pokemon.stats.map((stat: any) => (
+              <li key={stat.stat.name} className="capitalize">
+                {stat.stat.name}: {stat.base_stat}
+              </li>
+            ))}
+          </ul>
         </div>
 
-        <div className="flex flex-col gap-4 items-center w-full max-w-xs opacity-75">
-            <div className="bg-white rounded-2xl p-4 w-full text-center">
-                <h1 className="text-2xl font-bold text-center">Stats</h1>
-                <ul className="mt-2">
-                {pokemon.stats.map((stat: any) => (
-                    <li key={stat.stat.name} className="capitalize">
-                    {stat.stat.name}: {stat.base_stat}
-                    </li>
-                ))}
-                </ul>
-            </div>
-
-            <div className="bg-white rounded-2xl p-4 w-full text-center">
-                <h1 className="text-2xl font-bold text-center">Details</h1>
-                <ul className="mt-2">
-                <li>Weight: {pokemon.weight}</li>
-                <li>Height: {pokemon.height}</li>
-                <li>
-                    Abilities:{" "}
-                    {pokemon.abilities
-                    .map((abilityInfo: any) => abilityInfo.ability.name)
-                    .join(", ")}
-                </li>
-                <li>Base Experience: {pokemon.base_experience}</li>
-                </ul>
-            </div>
-
-            <div className="bg-white rounded-2xl p-4 w-full">
-                <h1 className="text-2xl font-bold text-center">Weaknesses</h1>
-                <p className="text-center">{Weaknesses.join(", ")}</p>
-            </div>
+        <div className="bg-white rounded-2xl p-4 w-full text-center">
+          <h1 className="text-2xl font-bold text-center">Details</h1>
+          <ul className="mt-2">
+            <li>Weight: {pokemon.weight}</li>
+            <li>Height: {pokemon.height}</li>
+            <li>
+              Abilities:{" "}
+              {pokemon.abilities
+                .map((abilityInfo: any) => abilityInfo.ability.name)
+                .join(", ")}
+            </li>
+            <li>Base Experience: {pokemon.base_experience}</li>
+          </ul>
         </div>
+
+        <div className="bg-white rounded-2xl p-4 w-full">
+          <h1 className="text-2xl font-bold text-center">Weaknesses</h1>
+          <p className="text-center">{Weaknesses.join(", ")}</p>
+        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export default PokemonDetail;
--- src/utils/pokemon.tsx
+++ src/utils/pokemon.tsx
@@ -1,62 +1,71 @@
 export interface Pokemon {
-    name: string;
-    type: string;
-    url: string;
-    id:number;
+  name: string;
+  type: string;
+  url: string;
+  id: number;
 }
 
-export async function getPokemonDetails(id: number){
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-    
+export async function getPokemonDetails(id: number) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
     if (!response.ok) {
-        throw new Error(`Pokemon with ID ${id} not found`)
+      throw new Error(`Pokemon with ID ${id} not found`);
     }
 
-    const data = await response.json()
+    const data = await response.json();
 
     return {
-        id: data.id,
-        name: data.name,
-        height: data.height,
-        weight: data.weight,
-        baseExperience: data.base_experience,
-        types: data.types.map((type: { type: { name: string } }) => type.type.name),
-        abilities: data.abilities.map((ability: { ability: { name: string } }) => ability.ability.name),
-        stats: data.stats.map((stat: { base_stat: number; stat: { name: string } }) => ({
-        name: stat.stat.name,
-        value: stat.base_stat,
-        })),
-    }
-    } catch (error) {
-        console.error(`Error fetching Pokemon details for ID ${id}:`, error)
-        return null
-    }
+      id: data.id,
+      name: data.name,
+      height: data.height,
+      weight: data.weight,
+      baseExperience: data.base_experience,
+      types: data.types.map(
+        (type: { type: { name: string } }) => type.type.name
+      ),
+      abilities: data.abilities.map(
+        (ability: { ability: { name: string } }) => ability.ability.name
+      ),
+      stats: data.stats.map(
+        (stat: { base_stat: number; stat: { name: string } }) => ({
+          name: stat.stat.name,
+          value: stat.base_stat,
+          moves: data.moves.map(
+            (move: { move: { name: string } }) => move.move.name
+          ),
+        })
+      ),
+    };
+  } catch (error) {
+    console.error(`Error fetching Pokemon details for ID ${id}:`, error);
+    return null;
+  }
 }
 
 export function getWeakness(types: string[]): string[] {
-    const typeWeaknesses: Record<string, string[]> = {
-        normal: ["fighting"],
-        fire: ["water", "ground", "rock"],
-        water: ["electric", "grass"],
-        electric: ["ground"],
-        grass: ["fire", "ice", "poison", "flying", "bug"],
-        ice: ["fire", "fighting", "rock", "steel"],
-        fighting: ["flying", "psychic", "fairy"],
-        poison: ["ground", "psychic"],
-        ground: ["water", "grass", "ice"],
-        flying: ["electric", "ice", "rock"],
-        psychic: ["bug", "ghost", "dark"],
-        bug: ["fire", "flying", "rock"],
-        rock: ["water", "grass", "fighting", "ground", "steel"],
-        ghost: ["ghost", "dark"],
-        dragon: ["ice", "dragon", "fairy"],
-        dark: ["fighting", "bug", "fairy"],
-        steel: ["fire", "fighting", "ground"],
-        fairy: ["poison", "steel"],
-    }
-    const allWeaknesses = types.flatMap((type) => typeWeaknesses[type] || [])
-    const uniqueWeaknesses = [...new Set(allWeaknesses)]
+  const typeWeaknesses: Record<string, string[]> = {
+    normal: ["fighting"],
+    fire: ["water", "ground", "rock"],
+    water: ["electric", "grass"],
+    electric: ["ground"],
+    grass: ["fire", "ice", "poison", "flying", "bug"],
+    ice: ["fire", "fighting", "rock", "steel"],
+    fighting: ["flying", "psychic", "fairy"],
+    poison: ["ground", "psychic"],
+    ground: ["water", "grass", "ice"],
+    flying: ["electric", "ice", "rock"],
+    psychic: ["bug", "ghost", "dark"],
+    bug: ["fire", "flying", "rock"],
+    rock: ["water", "grass", "fighting", "ground", "steel"],
+    ghost: ["ghost", "dark"],
+    dragon: ["ice", "dragon", "fairy"],
+    dark: ["fighting", "bug", "fairy"],
+    steel: ["fire", "fighting", "ground"],
+    fairy: ["poison", "steel"],
+  };
+  const allWeaknesses = types.flatMap((type) => typeWeaknesses[type] || []);
+  const uniqueWeaknesses = [...new Set(allWeaknesses)];
 
-    return uniqueWeaknesses
+  return uniqueWeaknesses;
 }
